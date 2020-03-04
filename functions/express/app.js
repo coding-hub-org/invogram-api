759497f40c94fb51ad7aaa86947418d0de19c05f
Quan Vo <votrungquan99@gmail.com>
2020-03-03 21:00:47 -0500

    add run locally with firebase

--- functions/express/app.js
+++ functions/express/app.js
@@ -7,6 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/firestore');
+var firebaseConfig = require('./firebase_config.json');
+firebase.initializeApp(firebaseConfig);
+
 var app = express();
 
 // view engine setup
@@ -24,18 +30,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
